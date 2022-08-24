dbd29831763bf3c1d365b32eaae50c6fb9d52a96
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-08-24 18:54:52 +0600

    Initial Commit

---
+++ PhotoWidget/PhotoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoWidgetApp.swift
+//  PhotoWidget
+//
+//  Created by Sopnil Sohan on 24/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
